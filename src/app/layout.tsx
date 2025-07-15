fded59fdc894c799cd742e4702a69efc307fa545
GalliumGa <galven.rivera@gmail.com>
2025-07-14 22:09:31 -0400

    removed unnecessary problems

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
 'use client'
 
 import './globals.css'
-import { Inter } from 'next/font/google'
 import Link from 'next/link';
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
